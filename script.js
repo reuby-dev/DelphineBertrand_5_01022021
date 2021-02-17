2d36c8647179c092d33b4aa32eb161beb9c47000
reuby-dev <delphineb57@gmail.com>
2021-02-17 13:52:35 +0100

    création des nounours et stockage dans un tableau

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 console.log('START');
 
+let peluche1 = {
+    name : 'Grarnaud',
+    description : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image : "back/images/teddy_1.jpg",
+    colors : ['red', 'yellow', 'green', 'blue'],
+    id : 123456789
+};
+
+let peluche2 = {
+    name : 'Gribaud',
+    description : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image : "back/images/teddy_2.jpg",
+    colors : ['red', 'yellow', 'green', 'blue'],
+    id : 123456789
+};
+
+let peluche3 = {
+    name : 'Grolivier',
+    description : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image : "back/images/teddy_3.jpg",
+    colors : ['red', 'yellow', 'green', 'blue'],
+    id : 123456789
+};
+
+let tousLesTeddys = [peluche1, peluche2, peluche3];
+
+console.log(tousLesTeddys)
 
 
 console.log('END');
