cef25dfe587794eb0c59549edb0347fbe74513d8
reuby-dev <delphineb57@gmail.com>
2021-02-18 17:34:10 +0100

    mise en place de fetch pour accès au tableau, création des cartes dynamiques

--- script.js
+++ script.js
@@ -1,32 +1,63 @@
-console.log('START');
+fetch('http://localhost:3000/api/teddies')
+    .then(function (response) {
+        return response.json();
+    }).then(function (teddy) {
+        let index = 0;
+        while (index < teddy.length) {
+            
+            /*ROW*/
+            let rowProduct = document.getElementById('product-container'); //récupère la row où il est nécessaire d'ajouter des éléments
 
-let peluche1 = {
-    name : 'Grarnaud',
-    description : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    image : "back/images/teddy_1.jpg",
-    colors : ['red', 'yellow', 'green', 'blue'],
-    id : 123456789
-};
+            /*COL*/
+            let colProduct = document.createElement('div'); //crée la colonne qui va contenir la card
+            rowProduct.appendChild(colProduct); //ajoute la colonne à la la ligne product-container
+            colProduct.classList.add('col-xs-6', 'col-sm-6', 'col-lg-4','mb-4'); //ajoute les classes à la colonne
+            
+            /*CARD*/
+            let card = document.createElement('div'); //crée la card
+            card.classList.add('card');//ajoute la classe à la card
+            colProduct.appendChild(card);//ajoute la card à la colonne
 
-let peluche2 = {
-    name : 'Gribaud',
-    description : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    image : "back/images/teddy_2.jpg",
-    colors : ['red', 'yellow', 'green', 'blue'],
-    id : 123456789
-};
+            /*IMG*/
+            let imgProduct = document.createElement('img'); //crée la balise img
+            imgProduct.src = teddy[index].imageUrl; //choisi l'image correspondante
+            imgProduct.classList.add('card-img-top', 'img-teddy'); //ajoute la classe à l'image
+            card.appendChild(imgProduct); //insère l'image dans le DOM
 
-let peluche3 = {
-    name : 'Grolivier',
-    description : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    image : "back/images/teddy_3.jpg",
-    colors : ['red', 'yellow', 'green', 'blue'],
-    id : 123456789
-};
+            /*CARD BODY */
+            let cardBody = document.createElement('div'); //crée la div qui va contenir le body de la card
+            cardBody.classList.add('card-body', 'd-flex', 'flex-column');//ajout les classes à card body
+            card.appendChild(cardBody);//ajoute le body à la card
 
-let tousLesTeddys = [peluche1, peluche2, peluche3];
+            /*CARD TOP IN BODY */
+            let topCard = document.createElement('div'); //crée la div qui va contenir le titre et le prix
+            topCard.classList.add('d-flex', 'flex-wrap', 'justify-content-between'); //ajoute les classes à la div
+            cardBody.appendChild(topCard); //ajoute la div au DOM
 
-console.log(tousLesTeddys)
+            /**TITLE */
+            let h2 = document.createElement('h2'); //crée le titre
+            h2.classList.add('card-title'); //ajoute la classe
+            h2.innerText = teddy[index].name; //sélectionne le titre dynamiquement
+            topCard.appendChild(h2); //ajoute le titre au DOM
 
+            /**PRICE */
+            let price = document.createElement('p'); //crée la balise p
+            price.classList.add('price');//ajoute la classe
+            price.innerText = teddy[index].price + ' €'; //sélectionne le prix dynamiquement et ajoute la devise
+            topCard.appendChild(price); //ajoute le prix au DOM
+            
 
-console.log('END');
+            /**DESCRIPTION */
+            let description = document.createElement('p'); //crée la balise p
+            description.innerText = teddy[index].description; // sélectionne la description dynamiquement
+            cardBody.appendChild(description); //ajoute la description au DOM
+
+            /**BUTTON */
+            let knowMore = document.createElement('button'); //crée un bouton
+            knowMore.classList.add('btn', 'bg-prim');
+            knowMore.innerHTML = 'En savoir plus';
+            cardBody.appendChild(knowMore);
+
+            index++;
+        }
+});
