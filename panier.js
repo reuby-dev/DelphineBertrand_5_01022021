e472095b810e959107e2560e2d0e107aa1ac87e7
reuby-dev <delphineb57@gmail.com>
2021-03-01 11:31:05 +0100

    mise à jour du panier sur la quantité et le prix associé au produit

--- panier.js
+++ panier.js
@@ -15,14 +15,54 @@ for (let i=0; i < cartContent.length; i++) {
   colName.innerText = cartContent[i].name;
   tRow.appendChild(colName);
 
-  /**colonne prix du produit */
-  let colPrice = document.createElement('td');
-  colPrice.innerText = divide(cartContent[i].price);
-  tRow.appendChild(colPrice);
-
   /**colonne quantité du produit */
   let colQty = document.createElement('td');
-  colQty.innerText = cartContent[i].quantity; //temporaire
   tRow.appendChild(colQty);
 
+  /**icone plus dans col quantité */
+  let plusIcon = document.createElement('p');
+  plusIcon.classList.add('fas', 'fa-plus', 'plus-icon');
+  colQty.appendChild(plusIcon);
+  plusIcon.addEventListener('click', function() {
+    //quand 'click' sur +, augmente la quantité de 1
+    cartContent[i].quantity++;
+    //calcule le prix total du produit
+    let priceProduct = cartContent[i].price * cartContent[i].quantity;
+    //met à jour le panier
+    localStorage.setItem('cart', JSON.stringify(cartContent));
+    //met à jour l'affichage de la quantité et le prix
+    productQuantity.innerText = cartContent[i].quantity;
+    colPrice.innerText = divide(priceProduct);
+  });  
+
+  /**Quantité du produit */
+  let productQuantity = document.createElement('p');
+  productQuantity.classList.add('quantity-product');
+  productQuantity.innerText = cartContent[i].quantity;
+  colQty.appendChild(productQuantity);
+
+  /**icone moins dans col quantité */
+  let minusIcon = document.createElement('p');
+  minusIcon.classList.add('fas', 'fa-minus', 'minus-icon');
+  colQty.appendChild(minusIcon);
+  minusIcon.addEventListener('click', function() {
+    //quand 'click' sur -, diminue la quantité de 1
+    cartContent[i].quantity--;
+    //calcule le prix total du produit
+    let priceProduct = cartContent[i].price * cartContent[i].quantity;
+    localStorage.setItem('cart', JSON.stringify(cartContent));
+    //met à jour l'affichage de la quantité et le prix
+    productQuantity.innerText = cartContent[i].quantity;
+    colPrice.innerText = divide(priceProduct);
+  });  
+
+  /**colonne prix du produit */
+  let priceProduct = cartContent[i].price * cartContent[i].quantity;
+  let colPrice = document.createElement('td');
+  colPrice.innerText = divide(priceProduct);
+  tRow.appendChild(colPrice);
+  
+  
+
+  
 }
