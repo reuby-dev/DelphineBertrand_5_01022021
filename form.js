c46b02b54006c55192c0a646431f17931e1e7ee8
reuby-dev <delphineb57@gmail.com>
2021-03-08 14:41:47 +0100

    nettoyage du panier et du formulaire.js

---
+++ form.js
@@ -0,0 +1,153 @@
+//sauvegarde l'orderId dans le local storage
+function saveOrderId(result) {
+    //récupère l'order Id depuis la réponse de l'api
+    let id = result.orderId;  
+
+    //stocke l'order Id dans le local storage pour pouvoir le récupérer dans la page de validation
+    let orderId = [];
+    orderId.push(id);
+    localStorage.setItem('orderId', JSON.stringify(orderId));
+  }
+
+//fonction validation des champs
+function validateField (fieldValue, parentContainerId) {
+    // Nettoyer les messages d'erreurs
+    let parentContainer = document.getElementById(parentContainerId);
+    for (let i = 0; i < parentContainer.children.length; i++) {
+        if (parentContainer.children[i].tagName === 'P') {
+            parentContainer.children[i].remove();
+        }
+    }
+
+    let error = false;
+
+    // On analyse le champ
+    if (fieldValue === "") {
+        error = true;
+        let parentContainer = document.getElementById(parentContainerId);
+        let emptyChildren = document.createElement('p');
+        emptyChildren.classList.add('text-danger', 'mb-0');
+        emptyChildren.innerText = 'Ce champ est obligatoire.';
+        parentContainer.appendChild(emptyChildren);
+    }
+    return error;
+}
+
+//fonction validation du panier
+function validateCart(cartValue) {
+    let error = false;
+    let containerCart = document.getElementById('container-cart');
+
+    // Nettoyer les messages d'erreurs du panier
+    for (let i = 0; i < containerCart.children.length; i++) {
+        if (containerCart.children[i].tagName === 'P' && containerCart.children[i].id  != 'subtotal-container') {
+            containerCart.children[i].remove();
+        }
+    }
+
+    //si le panier est vide
+    if (cartValue.length === 0 ) {
+        error = true;
+        let emptyCart = document.createElement('p');
+        emptyCart.classList.add('text-danger', 'fs-6');
+        emptyCart.innerText = 'Votre panier est vide, merci d\'y ajouter au moins un produit.';
+        containerCart.appendChild(emptyCart);
+  }
+  return error;
+}
+
+//Récupère les valeurs des champs du formulaire
+let firstName = document.getElementById('first-name').value;
+let lastName = document.getElementById('last-name').value;
+let email = document.getElementById('email').value;
+let address = document.getElementById('address').value;
+let postalCode = document.getElementById('postal-code').value;
+let city = document.getElementById('city').value;
+
+function validateForm (cartContent) {
+    
+    //erreur de tout le formulaire
+    let error = false;
+
+    //si le panier est vide
+    if (validateCart(cartContent) === true) {
+        error = true;
+    }
+
+    //si le champ firstname est vide
+    if (validateField (firstName, 'first-name-container') === true) {
+        error = true;
+    }
+
+    //si le champ nom est vide
+    if (validateField (lastName, 'last-name-container') === true) {
+        error = true;
+    }
+
+    //si le champ email est vide
+    if (validateField (email, 'email-container') === true) {
+        error = true;
+    }
+
+    //si le champ adresse est vide
+    if (validateField (address, 'address-container') === true) {
+        error = true;
+    }
+
+    //si le champ code postal est vide
+    if (validateField (postalCode, 'postal-code-container') === true) {
+        error = true;
+    }
+
+    //si le champ ville est vide
+    if (validateField (city, 'city-container') === true) {
+        error = true;
+    }  
+
+    return error;
+}
+
+/**REQUETE D'ENVOI DE LA COMMANDE */
+
+//quand on clique sur le bouton de confirmation, envoie la requete
+let buttonConfirm = document.getElementById('confirm-command');
+buttonConfirm.addEventListener('click', function(){
+    //recupère le contenu du local storage
+    let cartContent = JSON.parse(localStorage.getItem('cart'));
+    let globalError = validateForm (cartContent);
+    // Si j'ai une erreur après ma validation, je m'arrete
+    if (globalError === true) {
+        console.log("STOPPING because globalError is", globalError);
+        return;
+    }
+
+    /**RECUPERE LES IDENTIFIANTS DU PRODUIT DU PANIER */
+    let idProducts=[];
+    for (let i =0; i < cartContent.length; i++) {
+        result = cartContent[i].id;
+        idProducts.push(result);
+    }
+
+    /**REQUETE D'ENVOI */
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    let raw = JSON.stringify({"contact":{"firstName":firstName,"lastName":lastName,"address":address,"city":city,"email":email},"products":[idProducts]});
+
+    let requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+
+  fetch('http://localhost:3000/api/teddies/order/', requestOptions)
+  .then(function (response) {
+      return response.json();
+  }).then(function (result) {
+      console.log(result);  
+      saveOrderId(result);
+      document.location.href="validation.html";
+  })
+})
--- panier.js
+++ panier.js
@@ -25,17 +25,6 @@ for (let i=0; i < cartContent.length; i++) {
   plusIcon.classList.add('fas', 'fa-plus', 'plus-icon');
   colQty.appendChild(plusIcon);
 
-  plusIcon.addEventListener('click', function() {
-    //quand 'click' sur +, augmente la quantité de 1
-    cartContent[i].quantity++;
-    //met à jour le panier
-    localStorage.setItem('cart', JSON.stringify(cartContent));
-    //met à jour l'affichage de la quantité, le prix des articles et le prix total du panier
-    productQuantity.innerText = cartContent[i].quantity;
-    displaySubTotal (cartContent);
-    colPrice.innerText = divide(calcPriceProduct(cartContent[i].price, cartContent[i].quantity));
-  });  
-
   /**QUANTITE DU PRODUIT */
   let productQuantity = document.createElement('p');
   productQuantity.classList.add('quantity-product');
@@ -47,140 +36,57 @@ for (let i=0; i < cartContent.length; i++) {
   minusIcon.classList.add('fas', 'fa-minus', 'minus-icon');
   colQty.appendChild(minusIcon);
 
-  minusIcon.addEventListener('click', function() {
-    //quand 'click' sur -, diminue la quantité de 1
-    cartContent[i].quantity--;
-    //si la quantité du produit est inférieure ou égale à 0, supprimer le produit du localstorage et de l'affichage du tableau
-    if (cartContent[i].quantity <= 0) {
-      cartContent.splice([i].id, 1);
-      localStorage.setItem('cart', JSON.stringify(cartContent));
-      tBody.removeChild(tRow);
-    }
-    
-    //met à jour l'affichage de la quantité le prix des articles et le prix total du panier
-    localStorage.setItem('cart', JSON.stringify(cartContent));
-    console.log(i);
-    console.log(cartContent[i]);
-    
-    productQuantity.innerText = cartContent[i].quantity;
-    displaySubTotal (cartContent);
-    //calcule le prix total du produit
-    colPrice.innerText = divide(calcPriceProduct(cartContent[i].price, cartContent[i].quantity));
-  });
-
   /**COLONNE PRIX DU PRODUIT */
   let colPrice = document.createElement('td');
   colPrice.innerText = divide(calcPriceProduct(cartContent[i].price, cartContent[i].quantity));
   tRow.appendChild(colPrice); 
+
+  /**GESTION DE BOUTON + */
+  let productId = cartContent[i].id;
+  plusIcon.addEventListener('click', function() {
+    // Utiliser poductId pour trouver notre produit dans le panier
+    for(let j = 0; j < cartContent.length ; j++) {
+
+      // Si on trouve le produit
+      if(cartContent[j].id === productId) {
+        //quand 'click' sur +, augmente la quantité de 1
+        cartContent[j].quantity++;
+        //met à jour le panier
+        localStorage.setItem('cart', JSON.stringify(cartContent));
+        //met à jour l'affichage de la quantité, le prix des articles et le prix total du panier
+        productQuantity.innerText = cartContent[j].quantity;
+        colPrice.innerText = divide(calcPriceProduct(cartContent[j].price, cartContent[j].quantity));
+        displaySubTotal(cartContent);
+      }
+    }
+  });
+
+  /**GESTION DE BOUTON - */
+  minusIcon.addEventListener('click', function() {
+
+    for (let j = 0; j < cartContent.length; j++) {
+      if (productId === cartContent[j].id) {
+
+        //quand 'click' sur -, diminue la quantité de 1
+        cartContent[j].quantity--;
+
+        //si la quantité du produit est inférieure ou égale à 0, supprimer le produit du localstorage et de l'affichage du tableau
+        if (cartContent[j].quantity <= 0) {
+          cartContent.splice(j, 1);
+          tBody.removeChild(tRow);
+        } else {
+            productQuantity.innerText = cartContent[j].quantity;
+            //calcule le prix total du produit
+            colPrice.innerText = divide(calcPriceProduct(cartContent[j].price, cartContent[j].quantity));
+        }
+        //met à jour le local storage
+        localStorage.setItem('cart', JSON.stringify(cartContent));
+        //calcule le prix total du panier
+        displaySubTotal (cartContent);
+      }
+    }
+  });
 }
 
 /**AJOUTE LE TOTAL AU SPAN SUBTOTAL A L'ARRIVEE SUR LA PAGE DU PANIER*/
 displaySubTotal (cartContent);
-
-
-/**REQUETE D'ENVOI DE LA COMMANDE */
-
-//quand on clique sur le bouton de confirmation, envoie la requete
-let buttonConfirm = document.getElementById('confirm-command');
-buttonConfirm.addEventListener('click', function(){
-
-  //Récupère les valeurs des champs du formulaire
-  let firstName = document.getElementById('first-name').value;
-  let lastName = document.getElementById('last-name').value;
-  let email = document.getElementById('email').value;
-  let address = document.getElementById('address').value;
-  let postalCode = document.getElementById('postal-code').value;
-  let city = document.getElementById('city').value;
-
-  //conditions d'envoi
-
-  //si le panier est vide
-  if (cartContent.length === 0 ) {
-    let containerCart = document.getElementById('container-cart');
-    let emptyCart = document.createElement('p');
-    emptyCart.classList.add('text-danger', 'fs-6');
-    emptyCart.innerText = 'Votre panier est vide, merci d\'y ajouter au moins un produit.';
-    containerCart.appendChild(emptyCart);
-  }
-
-  //si le champ firstname est vide
-  if (firstName === "") {
-    let firstNameContainer = document.getElementById('first-name-container');
-    let emptyFirstName = document.createElement('p');
-    alertMessage(emptyFirstName, firstNameContainer);   
-  }
-
-  //si le champ nom est vide
-  if (lastName === "") {
-    let lastNameContainer = document.getElementById('last-name-container');
-    let emptyLastName = document.createElement('p');
-    alertMessage(emptyLastName, lastNameContainer);
-  }
-
-  //si le champ email est vide
-  if (email === "") {
-    let emailContainer = document.getElementById('email-container');
-    let emptyEmail = document.createElement('p');
-    alertMessage(emptyEmail, emailContainer);
-  }
-
-  //si le champ adresse est vide
-  if (address === "") {
-    let addressContainer = document.getElementById('address-container');
-    let emptyAddress = document.createElement('p');
-    alertMessage(emptyAddress, addressContainer);
-  }
-
-  //si le champ code postal est vide
-  if (postalCode === "") {
-    let postalCodeContainer = document.getElementById('postal-code-container');
-    let emptyPostalCode = document.createElement('p');
-    alertMessage(emptyPostalCode, postalCodeContainer);   
-  }
-
-  //si le champ ville est vide
-  if (city === "") {
-    let cityContainer = document.getElementById('city-container');
-    let emptyCity = document.createElement('p');
-    alertMessage(emptyCity, cityContainer);    
-  }
-  
-  /**RECUPERE LES IDENTIFIANTS DU PRODUIT DU PANIER */
-  let idProducts=[];
-  for (let i =0; i < cartContent.length; i++) {
-    result = cartContent[i].id;
-    idProducts.push(result);
-  }
-
-  /**REQUETE D'ENVOI */
-  let myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-
-  let raw = JSON.stringify({"contact":{"firstName":firstName,"lastName":lastName,"address":address,"city":city,"email":email},"products":[idProducts]});
-
-  let requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow'
-  };
-
-  function saveOrderId(result) {
-    //récupère l'order Id depuis la réponse de l'api
-    let id = result.orderId;  
-
-    //stocke l'order Id dans le local storage pour pouvoir le récupérer dans la page de validation
-    let orderId = [];
-    orderId.push(id);
-    localStorage.setItem('orderId', JSON.stringify(orderId));
-  }
-
-
-  fetch('http://localhost:3000/api/teddies/order/', requestOptions)
-  .then(function (response) {
-      return response.json();
-  }).then(function (result) {
-      console.log(result);  
-      saveOrderId(result);
-  })
-})
